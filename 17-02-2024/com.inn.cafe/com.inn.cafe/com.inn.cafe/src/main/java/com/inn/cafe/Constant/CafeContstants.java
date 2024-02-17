74d7879d69d9c0f2370bd70944707fe6fd38838d
jaydomadiya <jaydomadiya3101@gmail.com>
2024-02-17 10:18:25 +0530

    CafeManegementBackup

---
+++ 17-02-2024/com.inn.cafe/com.inn.cafe/com.inn.cafe/src/main/java/com/inn/cafe/Constant/CafeContstants.java
@@ -0,0 +1,14 @@
+package com.inn.cafe.Constant;
+
+
+ public class CafeContstants {
+
+    public static final String SOMTHING_WENT_WRONG = "Somthing Went Wrong";
+
+    public static final String INVALID_DATA = "Invalid Data";
+
+    public static final String UNAUTHORIZED_ACCESS = "Unauthoeized Access";
+
+     public static final String STORE_LOCATION = "D:\\CafeDownlodPdf";
+
+}
